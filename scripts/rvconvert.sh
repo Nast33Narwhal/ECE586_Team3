b1cf85be8affee09646874080e2d640875a57ac6
zorcat27 <bharwood@pdx.edu>
2022-02-27 22:18:04 -0800

    Modified script, first argument is a .o file, second argument is name of .mem file, no second argument defaults to prog.mem

--- scripts/rvconvert.sh
+++ scripts/rvconvert.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+if (($# < 2))
+then
+	eval /pkgs/riscv-gnu-toolchain/riscv-gnu-toolchain/bin/riscv64-unknown-linux-gnu-objdump -d $1 | grep -o '^[[:blank:]]*[[:xdigit:]]*:[[:blank:]][[:xdigit:]]*' > prog.mem
+else
+	eval /pkgs/riscv-gnu-toolchain/riscv-gnu-toolchain/bin/riscv64-unknown-linux-gnu-objdump -d $1 | grep -o '^[[:blank:]]*[[:xdigit:]]*:[[:blank:]][[:xdigit:]]*' > $2
+fi
 
-eval /pkgs/riscv-gnu-toolchain/riscv-gnu-toolchain/bin/riscv64-unknown-linux-gnu-objdump -d $1 | grep -o '^[[:blank:]]*[[:xdigit:]]*:[[:blank:]][[:xdigit:]]*' > prog.mem
