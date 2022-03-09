416c569a47e4df6b57f64318f8cb111f240bc626
drew-griffin <dgriffinseidel@gmail.com>
2022-03-08 20:33:37 -0800

    started adding breakpoint setting to UI. stepping added

--- src/userInterface/userInterface.c
+++ src/userInterface/userInterface.c
@@ -6,10 +6,14 @@
 #include "../memory/memory.h"
 #include "../decode/decode.h"
 
+#define MEM_SIZE 16384
+
 void displayUserInterface()
 {
     extern uint32_t PC; 
+    uint32_t breakpoint = 0; 
     char cmd[128] = "\0";
+
     while (strcmp(cmd, "run") && strcmp(cmd, "r"))
     {
         Printf("Enter command:");
@@ -23,9 +27,35 @@ void displayUserInterface()
             {
                 continue; //break condition set by loop, but don't want to print "unknown command"
             }
-            else if ((strcmp(cmd, "s")==0) || (strcmp(cmd, "step")==0)){
+            else if ((strcmp(cmd, "s")==0) || (strcmp(cmd, "step")==0))
+            {
                 setBreakpoint((PC + 4)/4); 
                 break;    
+            }
+            else if ((strcmp(cmd, "b")==0) || (strcmp(cmd, "break")==0))
+            {
+                Printf("Enter breakpoint location:"); 
+                Scanf("%u", &breakpoint); 
+                
+                if (breakpoint > MEM_SIZE * 4)
+                {
+                    Printf("Breakpoint too large!\n"); 
+                    continue; 
+                }
+                else if (breakpoint % 4 != 0)
+                {
+                    Printf("Breakpoint must be aligned (divisible by four)\n"); 
+                    continue;  
+                }
+                else if (breakpoint <= PC)
+                {
+                    Printf("Breakpoint must be greater than the current PC\n"); 
+                    continue; 
+                }
+                else{
+                    setBreakpoint(breakpoint/4); 
+                    break; 
+                }
             }
             else
             {
@@ -38,5 +68,5 @@ void displayUserInterface()
 void displayHelp()
 {
     Printf("List of available commands:");
-    Printf("run, help, step\n");
+    Printf("run, help, step, break\n");
 }
