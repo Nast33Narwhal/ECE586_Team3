d5a1626c896139bf85ead2fd4011c50c9807f084
drew-griffin <dgriffinseidel@gmail.com>
2022-03-08 20:00:51 -0800

    adding stepping to user interface

--- src/userInterface/userInterface.c
+++ src/userInterface/userInterface.c
@@ -3,9 +3,12 @@
 #include <stdio.h>
 #include "userInterface.h"
 #include "../wrappers/wrappers.h"
+#include "../memory/memory.h"
+#include "../decode/decode.h"
 
 void displayUserInterface()
 {
+    extern uint32_t PC; 
     char cmd[128] = "\0";
     while (strcmp(cmd, "run") && strcmp(cmd, "r"))
     {
@@ -15,6 +18,14 @@ void displayUserInterface()
             if ((strcmp(cmd, "h")==0) || (strcmp(cmd, "help")==0))
             {
                 displayHelp();
+            }
+            else if ((strcmp(cmd, "r")==0) || (strcmp(cmd, "run")==0))
+            {
+                continue; //break condition set by loop, but don't want to print "unknown command"
+            }
+            else if ((strcmp(cmd, "s")==0) || (strcmp(cmd, "step")==0)){
+                setBreakpoint((PC + 4)/4); 
+                break;    
             }
             else
             {
@@ -27,5 +38,5 @@ void displayUserInterface()
 void displayHelp()
 {
     Printf("List of available commands:");
-    Printf("run, help\n");
+    Printf("run, help, step\n");
 }
