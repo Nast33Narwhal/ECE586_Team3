cbb9e1e7178aeddd83d04a80d58fd638fa0e77c3
zorcat27 <bharwood@pdx.edu>
2022-03-12 00:12:56 -0800

    updated mul test file

--- tests/simple_tests/R_Type/mul/mul.s
+++ tests/simple_tests/R_Type/mul/mul.s
@@ -1,28 +1,30 @@
 # Multiply
 addi x5, x0, 5
 addi x6, x0, 6
-mul x10, x5, x6
+mul x10, x5, x6 #0x0000001E = 30
 addi x6, x0, -6
-mul x11, x5, x6
+mul x11, x5, x6 #0XFFFFFFE2 = -30
 
 # Multiply High
-mulh x12, x5, x6
+mulh x12, x5, x6 #0xFFFFFFFF_FFFFFFE2, store 0xFFFFFFFF
 
 # Multiply High Unsigned
-mulhu x13, x5, x6
+mulhu x13, x5, x6 #0x00000004_FFFFFFE2, store 0x00000004
 
 # Multiply High Signed Unsigned
-mulhsu x14, x5, x6
+mulhsu x14, x5, x6 #0x00000000_0000001E, store 0x00000000
 
 # Divide
-div x15, x12, x5
+div x15, x10, x5 #30/5=6 = 0x00000006
 
 # Divide Unsigned
-divu x16, x12, x5
+divu x16, x10, x6 #30/-6= 5 unsigned div 
 
 # Remainder
-rem x17, x12, x5
-rem x18, x6, x5
+rem x17, x10, x5 #30%5 = 6r0 = 0
+rem x18, x6, x5 #6%5 = 1r1 = 1
 
 # Remainder Unsigned
-rem x19, x6, x5
+rem x19, x6, x5 #6%5 = 1r1 = 1
+
+jr ra
