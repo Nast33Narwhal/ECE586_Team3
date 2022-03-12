2ed3c4d32b1df4c16c9284ccbe03bb475f8848f1
zorcat27 <bharwood@pdx.edu>
2022-03-12 00:32:36 -0800

    updating multiply test

--- tests/simple_tests/R_Type/mul/mul.s
+++ tests/simple_tests/R_Type/mul/mul.s
@@ -2,29 +2,33 @@
 addi x5, x0, 5
 addi x6, x0, 6
 mul x10, x5, x6 #0x0000001E = 30
-addi x6, x0, -6
-mul x11, x5, x6 #0XFFFFFFE2 = -30
+addi x7, x0, -6 #0xFFFFFFFA = -6
+mul x11, x5, x7 #0XFFFFFFE2 = -30
 
 # Multiply High
-mulh x12, x5, x6 #0xFFFFFFFF_FFFFFFE2, store 0xFFFFFFFF
+mulh x12, x5, x6 #5*6 = 0x00000000_0000001E
+mulh x13, x5, x7 #5*-6= 0x5 * 0xFFFFFFFA = 0xFFFFFFFF_FFFFFFE2, store 0xFFFFFFFF
 
 # Multiply High Unsigned
-mulhu x13, x5, x6 #0x00000004_FFFFFFE2, store 0x00000004
+mulhu x14, x5, x6 #5*6 = 0x5 * 0x6 = 0x00000000_0000001E = 0x0
+mulhu x15, x5, x7 #5*-6 = 0x5 * 0xFFFFFFFA = 0x00000004_FFFFFFE2, store 0x00000004
 
 # Multiply High Signed Unsigned
-mulhsu x14, x5, x6 #0x00000000_0000001E, store 0x00000000
+mulhsu x16, x7, x5 #0xFFFFFFFA * 0x5 = 0xFFFFFFFF
+mulhsu x17, x5, x6 #0x5 * 0x6 = 0x00000000_0000001E, store 0x00000000
 
 # Divide
-div x15, x10, x5 #30/5=6 = 0x00000006
+div x18, x10, x5 # 30/5=6 = 0x00000006
+div x19, x10, x7 # 30/-6 = -5 = 0xFFFFFFFB
 
 # Divide Unsigned
-divu x16, x10, x6 #30/-6= 5 unsigned div 
+divu x20, x7, x5 #0xFFFFFFFA / 0x5 = 0x33333332 
 
 # Remainder
-rem x17, x10, x5 #30%5 = 6r0 = 0
-rem x18, x6, x5 #6%5 = 1r1 = 1
+rem x21, x10, x5 #30%5 = 6r0 = 0
+rem x22, x6, x5 #6%5 = 1r1 = 1
 
 # Remainder Unsigned
-rem x19, x6, x5 #6%5 = 1r1 = 1
+rem x23, x7, x6 #0xFFFFFFFA % 0x6 = r4 = 4
 
 jr ra
