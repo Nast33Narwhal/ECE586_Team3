58bdd651421c47a8dcb437a148bc4b60182e5275
drew-griffin <dgriffinseidel@gmail.com>
2022-03-12 21:57:57 -0800

    another branch test added

---
+++ tests/simple_tests/BJ_Type/branch/branch2.s
@@ -0,0 +1,165 @@
+	.file	"branch2.c"
+	.option pic
+	.text
+	.align	2
+	.globl	main
+	.type	main, @function
+main:
+	addi	sp,sp,-32
+	sw	ra,28(sp)
+	sw	s0,24(sp)
+	addi	s0,sp,32
+	li	a5,-6
+	sw	a5,-20(s0)
+	j	.L2
+.L8:
+	sw	zero,-32(s0)
+	j	.L3
+.L7:
+	lw	a4,-20(s0)
+	lw	a5,-32(s0)
+	ble	a4,a5,.L4
+	call	branch_greater@plt
+	j	.L5
+.L4:
+	lw	a4,-20(s0)
+	lw	a5,-32(s0)
+	bne	a4,a5,.L6
+	call	branch_equal@plt
+	j	.L5
+.L6:
+	lw	a4,-20(s0)
+	lw	a5,-32(s0)
+	bge	a4,a5,.L5
+	call	branch_less@plt
+.L5:
+	lw	a5,-20(s0)
+	addi	a5,a5,1
+	sw	a5,-20(s0)
+.L3:
+	lw	a4,-32(s0)
+	li	a5,6
+	ble	a4,a5,.L7
+	lw	a5,-20(s0)
+	addi	a5,a5,1
+	sw	a5,-20(s0)
+.L2:
+	lw	a4,-20(s0)
+	li	a5,6
+	ble	a4,a5,.L8
+	li	a5,-6
+	sw	a5,-24(s0)
+	j	.L9
+.L15:
+	sw	zero,-28(s0)
+	j	.L10
+.L14:
+	lw	a5,-28(s0)
+	lw	a4,-24(s0)
+	bleu	a4,a5,.L11
+	call	branch_greater@plt
+	j	.L12
+.L11:
+	lw	a5,-28(s0)
+	lw	a4,-24(s0)
+	bne	a4,a5,.L13
+	call	branch_equal@plt
+	j	.L12
+.L13:
+	lw	a5,-28(s0)
+	lw	a4,-24(s0)
+	bgeu	a4,a5,.L12
+	call	branch_less@plt
+.L12:
+	lw	a5,-24(s0)
+	addi	a5,a5,1
+	sw	a5,-24(s0)
+.L10:
+	lw	a4,-28(s0)
+	li	a5,6
+	ble	a4,a5,.L14
+	lw	a5,-24(s0)
+	addi	a5,a5,1
+	sw	a5,-24(s0)
+.L9:
+	lw	a4,-24(s0)
+	li	a5,6
+	bleu	a4,a5,.L15
+	li	a5,0
+	mv	a0,a5
+	lw	ra,28(sp)
+	lw	s0,24(sp)
+	addi	sp,sp,32
+	jr	ra
+	.size	main, .-main
+	.align	2
+	.globl	branch_greater
+	.type	branch_greater, @function
+branch_greater:
+	addi	sp,sp,-16
+	sw	s0,12(sp)
+	addi	s0,sp,16
+#APP
+# 52 "branch2.c" 1
+	addi x8, x0, 1
+# 0 "" 2
+# 53 "branch2.c" 1
+	addi x9, x0, 0x00000000
+# 0 "" 2
+# 54 "branch2.c" 1
+	addi x10, x0, 0x00000000
+# 0 "" 2
+#NO_APP
+	nop
+	lw	s0,12(sp)
+	addi	sp,sp,16
+	jr	ra
+	.size	branch_greater, .-branch_greater
+	.align	2
+	.globl	branch_equal
+	.type	branch_equal, @function
+branch_equal:
+	addi	sp,sp,-16
+	sw	s0,12(sp)
+	addi	s0,sp,16
+#APP
+# 59 "branch2.c" 1
+	addi x9, x0, 1
+# 0 "" 2
+# 60 "branch2.c" 1
+	addi x8, x0, 0x00000000
+# 0 "" 2
+# 61 "branch2.c" 1
+	addi x10, x0, 0x00000000
+# 0 "" 2
+#NO_APP
+	nop
+	lw	s0,12(sp)
+	addi	sp,sp,16
+	jr	ra
+	.size	branch_equal, .-branch_equal
+	.align	2
+	.globl	branch_less
+	.type	branch_less, @function
+branch_less:
+	addi	sp,sp,-16
+	sw	s0,12(sp)
+	addi	s0,sp,16
+#APP
+# 67 "branch2.c" 1
+	addi x10, x0, 1
+# 0 "" 2
+# 68 "branch2.c" 1
+	addi x8, x0, 0x00000000
+# 0 "" 2
+# 69 "branch2.c" 1
+	addi x9, x0, 0x00000000
+# 0 "" 2
+#NO_APP
+	nop
+	lw	s0,12(sp)
+	addi	sp,sp,16
+	jr	ra
+	.size	branch_less, .-branch_less
+	.ident	"GCC: (GNU) 9.2.0"
+	.section	.note.GNU-stack,"",@progbits
